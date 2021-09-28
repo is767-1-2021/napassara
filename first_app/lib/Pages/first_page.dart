5f30ec64856953a0177993437ecb30ad0abd0df0
Napassara Toyon <napassara.toyon@gmail.com>
2021-09-28 10:11:31 +0700

    Add Provider to form

--- first_app/lib/Pages/first_page.dart
+++ first_app/lib/Pages/first_page.dart
@@ -1,34 +1,40 @@
-import 'package:first_app/Pages/second_page.dart';
+import 'package:first_app/models/first_form_model.dart';
+import 'package:first_app/pages/second_page.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class FirstPage extends StatefulWidget {
+class FirstPage extends StatefulWidget{
   @override
   _FirstPageState createState() => _FirstPageState();
 }
 
 class _FirstPageState extends State<FirstPage> {
-  String? _formData = 'Please Click to fill the form';
-
+  String? _formData = 'Please click to fill the form';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('First Page'),
+        title: Text('First Page First Page'),
         actions: [
-          IconButton(onPressed: () {}, icon: Icon(Icons.arrow_forward)),
-          IconButton(onPressed: () {}, icon: Icon(Icons.agriculture)),
+          IconButton(onPressed: (){}, icon: Icon(Icons.arrow_forward)),
+          IconButton(onPressed: (){}, icon: Icon(Icons.agriculture)),
           IconButton(
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => SecondPage()));
-              },
-              icon: Icon(Icons.bus_alert)),
+            onPressed: (){
+              Navigator.push(context, 
+                MaterialPageRoute(
+                  builder: (context) => SecondPage(), 
+                ),
+              );
+            },
+            icon: Icon(Icons.bus_alert)
+          ),
           IconButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/3');
-              },
-              icon: Icon(Icons.medication)),
-          IconButton(onPressed: () {}, icon: Icon(Icons.food_bank)),
+            onPressed: (){
+              Navigator.pushNamed(context, '/3');
+            },
+            icon: Icon(Icons.medication)
+          ),
+          IconButton(onPressed: (){}, icon: Icon(Icons.food_bank)),
         ],
       ),
       body: Center(
@@ -36,21 +42,18 @@ class _FirstPageState extends State<FirstPage> {
           children: [
             Padding(
               padding: EdgeInsets.all(20.0),
-              child: Text('$_formData'),
+              child: Consumer<FirstFormModel>(
+                builder: (context, form, child){
+                  return Text('${form.firstName} ${form.lastName} ${form.age}' );
+                },
+              ),
             ),
             ElevatedButton(
-              //*async & await use for make sure that we will get the response value to display
-              onPressed: () async {
-                var response = await Navigator.pushNamed(context, '/6');
-
-                if (response != null && !response.toString().isEmpty) {
-                  setState(() {
-                    _formData = response.toString();
-                  });
-                }
-              },
-              child: Text('Fill This Form please'),
-            ),
+              onPressed: () {
+                Navigator.pushNamed(context, '/6');
+              }, 
+              child: Text('Fill this form please'),
+              ),
           ],
         ),
       ),
