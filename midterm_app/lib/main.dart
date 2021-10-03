b0d226faf1cdda879a8e5f62369a14e662116266
Napassara Toyon <napassara.toyon@gmail.com>
2021-10-03 17:11:41 +0700

    Midterm exam

--- midterm_app/lib/main.dart
+++ midterm_app/lib/main.dart
@@ -31,10 +31,149 @@ class MyApp extends StatelessWidget {
             bodyText2: TextStyle(color: Colors.purple),
           ),
         ),
-        initialRoute: '/1',
+        initialRoute: '/0',
         routes: <String, WidgetBuilder>{
+          '/0': (context) => MenuPage(),
           '/1': (context) => FirstPage(),
           '/2': (context) => SecondPage(),
+          '/3': (context) => ThirdPage(),
+          '/4': (context) => FourthPage(),
+          '/5': (context) => FifthPage(),
+          '/6': (context) => SixthPage(),
         });
   }
 }
+
+class MenuPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Menu'),
+      ),
+      
+      body: GridView.count(
+        crossAxisCount: 2,
+        children: List.generate(6, (index) {
+          return InkWell(
+            onTap: () {
+              Navigator.pushNamed(context, '/${index + 1}');
+            },
+          child: Container(
+              margin: EdgeInsets.all(20.0),
+              decoration: BoxDecoration(
+                color: Theme.of(context).accentColor,
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              child: Center(
+                child: Text(
+                  'Item ${index + 1}',
+                  style: Theme.of(context).textTheme.headline5
+                ),
+              ),
+            ),
+          );
+        }),
+      ),
+    );
+  }
+}
+
+class ThirdPage extends StatefulWidget{
+  @override
+  _ThirdPageState createState() => _ThirdPageState();
+}
+class _ThirdPageState extends State<ThirdPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Third Page'),
+        actions: [
+          IconButton(
+            onPressed: (){
+              Navigator.pushNamed(context, '/0');
+            },
+            icon: Icon(Icons.arrow_back)
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.build_sharp),
+        onPressed: () {},
+      ),
+    );
+  }
+}
+
+class FourthPage extends StatefulWidget{
+  @override
+  _FourthPageState createState() => _FourthPageState();
+}
+class _FourthPageState extends State<FourthPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Fourth Page'),
+        actions: [
+          IconButton(
+            onPressed: (){
+              Navigator.pushNamed(context, '/0');
+            },
+            icon: Icon(Icons.arrow_back)
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+
+class FifthPage extends StatefulWidget{
+  @override
+  _FifthPageState createState() => _FifthPageState();
+}
+class _FifthPageState extends State<FifthPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Fifth Page'),
+        actions: [
+          IconButton(
+            onPressed: (){
+              Navigator.pushNamed(context, '/0');
+            },
+            icon: Icon(Icons.arrow_back)
+          ),
+        ],
+      ),
+    );
+  }
+}
+              
+class SixthPage extends StatefulWidget{
+  @override
+  _SixthPageState createState() => _SixthPageState();
+}
+class _SixthPageState extends State<SixthPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sixth Page'),
+        actions: [
+          IconButton(
+            onPressed: (){
+              Navigator.pushNamed(context, '/0');
+            },
+            icon: Icon(Icons.arrow_back)
+          ),
+        ],
+      ),
+    );
+  }
+}
+
